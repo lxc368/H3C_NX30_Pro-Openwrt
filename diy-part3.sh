30909ba0155ca7d82a4b59e953944927bf278bed
lxc368 <lxc369@outlook.com>
2024-07-19 17:36:14 +0800

    Add files via upload

---
+++ diy-part3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (Before Update feeds)
+#
+ 
+mv banner openwrt/package/base-files/files/etc/banner
